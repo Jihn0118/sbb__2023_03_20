203317d476049f8d7d1cac181e23326f2ad23e05
Jihn0118 <lantern50@tukorea.ac.kr>
2023-03-21 10:49:56 +0900

    테스트 케이스 독립적(beforeEach)

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -5,6 +5,7 @@ import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,24 @@ class SbbApplicationTests {
 
 	@Autowired
 	private AnswerRepository aR;
+
+	@BeforeEach
+	void beforeEach(){
+		qR.deleteAll();
+		qR.clearAutoIncrement();
+		// 첫번째 질문 저장
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.qR.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문임다");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.qR.save(q2);
+	}
 
 	@Test
 	void testJpa() {
