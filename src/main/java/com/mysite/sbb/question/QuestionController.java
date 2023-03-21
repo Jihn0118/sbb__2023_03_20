21b263d1a66ff45c6318abd022160706f84812ac
Jihn0118 <lantern50@tukorea.ac.kr>
2023-03-21 15:46:26 +0900

    질문 상세 페이지

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,20 +4,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
     private final QuestionService qS;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.qS.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+        Question q = this.qS.getQuestion(id);
+        model.addAttribute("question", q);
+        return "question_detail";
     }
 }
--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,6 +12,10 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
 
+    @Transactional
+    @Modifying
+    // @Modifying: 만약 아래 쿼리가 SELECT가 아니라면 이 어노테이션을 붙여야한다.
     @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
+    // nativeQuery = true 여야 MySQL 쿼리문법 사용 가능
     void clearAutoIncrement();
 }
